bf3feb8eb898a6ced0ec5256e20b91299549cd39
kz-create <mr.kzak315@gmail.com>
2023-07-06 22:54:48 +0900

    uninstall recoil and other package

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import { ComboBox } from "./ComboBox";
 import "./App.css";
-import { RecoilRoot } from "recoil";
 import { worker } from "../mock/browser";
 
 worker.start();
 
 function App() {
-  return (
-    <RecoilRoot>
-      <ComboBox />
-    </RecoilRoot>
-  );
+  return <ComboBox />;
 }
 
 export default App;
